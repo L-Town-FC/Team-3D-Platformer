0c23a67c43a6b1da6deca75b5fa2b8d2421b5d4d
ColinKrohto <7willythekid@gmail.com>
2025-12-16 11:38:30 -0500

    Rewrote enemy chase to use rb.Move instead of rb.linearVelocity
    Rigidbodies should not be moved using rb.linearVelocity as this will override and break physics calculations. Either use rb.Move or rb.AddForce. Switch to rb.Move since its what the player and platforms use

--- Assets/Scripts/World/Enemies/EnemyChase.cs
+++ Assets/Scripts/World/Enemies/EnemyChase.cs
@@ -8,10 +8,18 @@ public class EnemyChase : MonoBehaviour
     [SerializeField] private float stopDistance = 0.75f;
 
     private Rigidbody rb;
+    bool isGrounded;
+    Vector3 gravity = Vector3.zero;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //zeroes out linar velocity in case some unaccounted velocity is added and messes up movement
+        rb.linearVelocity = Vector3.zero;
     }
 
     private void FixedUpdate()
@@ -27,15 +35,28 @@ public class EnemyChase : MonoBehaviour
         float dist = toPlayer.magnitude;
         if (dist <= stopDistance || dist < 0.001f)
         {
-            rb.linearVelocity = new Vector3(0f, rb.linearVelocity.y, 0f);
             return;
         }
 
-        Vector3 dir = toPlayer / dist;
+        Vector3 dir = toPlayer.normalized;
 
-        // Move using physics so collisions with ground behave naturally
-        Vector3 desired = dir * moveSpeed;
-        rb.linearVelocity = new Vector3(desired.x, rb.linearVelocity.y, desired.z);
+        gravity += Vector3.down;
+        if (isGrounded)
+        {
+            gravity = Vector3.zero;
+        }
+;
+        //rb.linearVelocity = new Vector3(desired.x, rb.linearVelocity.y, desired.z);
+        rb.MovePosition(rb.position + (dir * moveSpeed + gravity) * Time.fixedDeltaTime);
+
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //if its touching anything it must be grounded
+        //true in almost all cases and I dont feel like putting in the extra effort to cover edge cases rightnow
+        isGrounded = true;
     }
 
     public void SetPlayer(Transform t)
