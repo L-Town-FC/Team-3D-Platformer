1b136d54bafad628afc3adeebfd4d2491b9bd971
ColinKrohto <7willythekid@gmail.com>
2025-12-21 13:15:56 -0500

    Camera sens and tilt range are now script modifiable
    Camera script can now be used to set the camera sensitivity and tilt range

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -6,42 +6,37 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField] private PlayerInput input;
 
     // Drag your active CinemachineCamera here (the one in your screenshot)
-    [SerializeField] private CinemachineCamera cinemachineCamera;
-
-    // Drag the CameraTarget transform here (the same one shown in Tracking Target)
-    [SerializeField] private Transform cameraTarget;
+    [SerializeField] GameObject cinemachineCamera;
+    private CinemachineInputAxisController inputAxisController;
+    private CinemachineOrbitalFollow orbitFollow;
 
     [SerializeField] private Vector2 minAndMaxCameraTilt = new Vector2(-60f, 75f);
-    [SerializeField] private Vector2 horizontalAndVerticalCameraSensitivity = new Vector2(10f, 10f);
+    [SerializeField] private Vector2 horizontalAndVerticalCameraSensitivity = new Vector2(1f, 1f);
 
-    private float yaw;
-    private float pitch;
 
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
-        if (cameraTarget != null)
-        {
-            Vector3 e = cameraTarget.rotation.eulerAngles;
-            yaw = e.y;
-            pitch = e.x;
-        }
+        inputAxisController = cinemachineCamera.GetComponent<CinemachineInputAxisController>();
+        orbitFollow = cinemachineCamera.GetComponent<CinemachineOrbitalFollow>();
     }
 
     private void Update()
     {
-        if (input == null || cameraTarget == null) { return; }
+        
+    }
 
-        Vector2 look = input.cameraInput;
+    private void OnValidate()
+    {
+        //this will have to be moved out of OnValidate when we want these values to be modifiable by UI
+        if(inputAxisController != null && orbitFollow != null)
+        {
+            //sets camera sensitivity
+            inputAxisController.GetController("Look Orbit X").Input.Gain = horizontalAndVerticalCameraSensitivity.x;
+            inputAxisController.GetController("Look Orbit Y").Input.Gain = horizontalAndVerticalCameraSensitivity.y;
 
-        yaw += look.x * horizontalAndVerticalCameraSensitivity.x * Time.deltaTime;
-        pitch -= look.y * horizontalAndVerticalCameraSensitivity.y * Time.deltaTime;
-
-        // clamp pitch (tilt)
-        pitch = Mathf.Clamp(pitch, minAndMaxCameraTilt.x, minAndMaxCameraTilt.y);
-
-        //this is really bad and shouldnt be done
-        cameraTarget.rotation = Quaternion.Euler(pitch, yaw, 0f);
+            //set camera tilt constaints
+            orbitFollow.VerticalAxis.Range = minAndMaxCameraTilt;
+        }
     }
 }
