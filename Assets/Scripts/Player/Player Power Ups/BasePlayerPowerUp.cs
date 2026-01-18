f9a1fd7062a55b116b9f3882e7e0457cb9e6c944
ColinKrohto <7willythekid@gmail.com>
2026-01-18 11:47:42 -0500

    Started setting up power up system

---
+++ Assets/Scripts/Player/Player Power Ups/BasePlayerPowerUp.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class BasePlayerPowerUp : MonoBehaviour
+{
+    protected virtual bool isPowerUpEnabled { get; set; }
+    string playerTag = "Player";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    protected virtual void OnCollect()
+    {
+        //power up collection effect
+        //time stop?
+    }
+
+    protected void UpdatePlayerPrefs(string powerupName)
+    {
+        //0 = does not have power up, 1 = has power up
+        PlayerPrefs.SetInt(powerupName, 1);
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(playerTag)){
+            return;
+        }
+    }
+}
